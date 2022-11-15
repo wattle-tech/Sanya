3564f3ce45bde5cacfff792905eab3c345a5cacb
Andrew Kovalenko <name4doc@yandex.ru>
2022-11-15 21:18:44 +0300

    DC

--- api/recognize.py
+++ api/recognize.py
@@ -9,11 +9,10 @@ def recognition ():
             r.adjust_for_ambient_noise(source=mic)
             audio = r.listen(source=mic)
             recogn = r.recognize_google(audio_data=audio, language='ru-RU')
+            return str(recogn)
     except sr.UnknownValueError:
         pass
     except sr.RequestError:
         pass
-
-    return str(recogn)
 
 recognition()
