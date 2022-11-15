5bab655a3538eab368e164d549e22289782fbcaa
Andrew Kovalenko <name4doc@yandex.ru>
2022-11-15 20:43:29 +0300

    recognition FC

--- api/recognize.py
+++ api/recognize.py
@@ -1,16 +1,19 @@
 import speech_recognition as sr
 import pyaudio
 
-r = sr.Recognizer()
+def recognition ():
+    r = sr.Recognizer()
 
-try:
-    with sr.Microphone() as mic:
-        r.adjust_for_ambient_noise(source=mic)
-        audio = r.listen(source=mic)
-        recogn = r.recognize_google(audio_data=audio, language='ru-RU')
-except sr.UnknownValueError:
-    pass
-except sr.RequestError:
-    pass
+    try:
+        with sr.Microphone() as mic:
+            r.adjust_for_ambient_noise(source=mic)
+            audio = r.listen(source=mic)
+            recogn = r.recognize_google(audio_data=audio, language='ru-RU')
+    except sr.UnknownValueError:
+        pass
+    except sr.RequestError:
+        pass
 
-print (str(recogn))
+    return str(recogn)
+
+recognition()
