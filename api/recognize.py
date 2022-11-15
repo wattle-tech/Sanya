4ff3b6b0cabcc761975a713b82bd75c6f7dc568a
Andrew Kovalenko <name4doc@yandex.ru>
2022-11-15 20:33:16 +0300

    Speech Recognition (beta) done

--- api/recognize.py
+++ api/recognize.py
@@ -1,6 +1,16 @@
 import speech_recognition as sr
+import pyaudio
 
-r=sr.Recognizer() 
+r = sr.Recognizer()
 
-def recognizer ():
+try:
+    with sr.Microphone() as mic:
+        r.adjust_for_ambient_noise(source=mic)
+        audio = r.listen(source=mic)
+        recogn = r.recognize_google(audio_data=audio, language='ru-RU')
+except sr.UnknownValueError:
     pass
+except sr.RequestError:
+    pass
+
+print (str(recogn))
