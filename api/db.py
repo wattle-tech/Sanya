9d263e6936af01153a670c8407c6da1ab20a24a8
Mr.Far <danila.ronis@gmail.com>
2022-11-17 17:49:02 +0300

    CMAC

--- api/db.py
+++ api/db.py
@@ -4,17 +4,17 @@ import time
 db = sqlite3.connect("./data/db.db")
 cr = db.cursor()
 
-class AlarmClock:
+class AlarmClock: #создаём класс
 
     @classmethod
     def add(cls, name: str, time: int, sound_path: str = None):
-        cr.execute("INSERT INTO clock (name, time, sound) VALUES (?,?,?)",
-                [name, time, sound_path])
+        cr.execute("INSERT INTO clock (name, time, sound) VALUES (?,?,?)", #в таюлицу clock добавляем name, time, sound
+                [name, time, sound_path]) #Добавляем в бд данные
         db.commit()
 
     @classmethod
     def get_clocks(cls):
-        clocks = cr.execute("SELECT name FROM clock")
-        return clocks
+        clocks = cr.execute("SELECT name FROM clock") #получаем все данные из столбца name
+        return clocks 
 
 
