8c0baeca09d74cb60bf8b80dbdf6085f49af7ada
Mr.Far <danila.ronis@gmail.com>
2022-12-21 20:04:29 +0300

    fix: sanya model loads everytime in func `say`

--- sanya/assistant.py
+++ sanya/assistant.py
@@ -9,42 +9,27 @@ __all__ =(
 
 class Assistant:
     def __init__(self) -> None:
+        self.__model = 0
         self.__sample_rate = 48000
         self.__noise = 0
-        
 
-    def __en_model(self):
+    
+    def __en_voice(self, text: str):
         #Модель голоса на английском
+        global model_en
         language_en = 'en'
         model_id_en = 'v3_en'
         speaker_en = 'en_1'
         device_en = torch.device('cpu') # gpu or cpu
-
-        model_en, null_1 = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                            model='silero_tts',
-                                            language=language_en,
-                                            speaker=model_id_en)
-        model_en.to(device_en)
-        return model_en
-        
-
-    def __ru_model(self, text: str):
-        #Модель голоса
-        language = 'ru'
-        model_id = 'v3_1_ru'
-        speaker = 'eugene' #aidar, baya, kseniya, xenia, eugene, random
-        device = torch.device('cpu') # gpu or cpu
-
-        model, null = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                            model='silero_tts',
-                                            language=language,
-                                            speaker=model_id)
-        model.to(device)
-        return model
-    
-    def __en_voice(self, text: str):
+        if self.__model == 0:
+            model_en, null = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                                                model='silero_tts',
+                                                language=language_en,
+                                                speaker=model_id_en)
+            model_en.to(device_en)
+            self.__model += 1
         audio_en = model_en.apply_tts(text=text,
-                                    speaker='en_1',
+                                    speaker=speaker_en,
                                     sample_rate=self.__sample_rate,
                                     put_accent=True,
                                     put_yo=True)
@@ -54,8 +39,22 @@ class Assistant:
         
 
     def __ru_voice(self, text: str):
+        #Модель голоса
+        global model
+        language = 'ru'
+        model_id = 'v3_1_ru'
+        speaker = 'eugene' #aidar, baya, kseniya, xenia, eugene, random
+        device = torch.device('cpu') # gpu or cpu
+
+        if self.__model == 0:
+            model, null = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                                                model='silero_tts',
+                                                language=language,
+                                                speaker=model_id)
+            model.to(device)
+            self.__model += 1
         audio = model.apply_tts(text=text,
-                                speaker='eugene',
+                                speaker=speaker,
                                 sample_rate=self.__sample_rate,
                                 put_accent=True,
                                 put_yo=True)
@@ -69,9 +68,10 @@ class Assistant:
         lang = "ru"
 
         if lang == "en":
-            self.__en_model(text)
+            self.__en_voice(text)
+            
         if lang == "ru":
-            self.__ru_model(text)
+            self.__ru_voice(text)
         else:
             raise ValueError("This language isn't supported!")
     
