2d7cf35546a8bd703b2007c20e3d4fb982a79c8b
Mr.Far <danila.ronis@gmail.com>
2022-12-22 14:54:42 +0300

    add `weatherp`

---
+++ api/weatherp.py
@@ -0,0 +1,43 @@
+import random
+
+__all__=("weather_request")
+
+plusforty = ["одевайтесь легче", "можете сходить на пляж. А нет, отмена, река пересохла.", "Кстати, а что если выйти на улицу голым? Сейчас проверим!", "жарковато, не находите?"]
+plustwentyfive = ["время для летней майки!", "ну что, идёте открывать купальный сезон?", "жарковато..."]
+plustwenty = ["ну вот и потеплело!", "одевайтесь легче."]
+plusten = ["накиньте что нибудь", "у вас же была желетка?", "прямо как в Лондоне!", "как на счёт осеннего пальто?"]
+plusfive = ["лучше одется потеплее", "накиньте что нибудь", "одевайтесь потеплее", "холодновато", "холодновато, не находите?"]
+pluszero = ["лучше надеть куртку", "наденьте шарф и шапку", "надеюсь у вас есть куртка?"]
+belowzerofive = ["уже не лето, не забудь про куртку", "не превратись в сосульку!", "одевайся теплее", "у тебя же есть куртка?", "замёрзнешь!"]
+
+
+def weather_request (num: int):
+    if num <= -5:
+        v = random.randrange(0, len(belowzerofive))
+        return belowzerofive[v]
+
+    if num <= 5 and num > -5:
+        v = random.randrange(0, len(pluszero))
+        return pluszero[v]
+    
+    if num >= 5 and num < 10:
+        v = random.randrange(0, len(plusfive))
+        return plusfive[v]
+
+    if num >= 10 and num < 20:
+        v = random.randrange(0, len(plusten))
+        return plusten[v]
+
+    if num >= 20 and num < 25:
+        v = random.randrange(0, len(plustwenty))
+        return plustwenty[v]
+
+    if num >= 25 and num < 40:
+        v = random.randrange(0, len(plustwentyfive))
+        return plustwentyfive[v]
+
+    if num >= 40:
+        v = random.randrange(0, len(plusforty))
+        return plusforty[v]
+
+print(weather_request(int(input())))
